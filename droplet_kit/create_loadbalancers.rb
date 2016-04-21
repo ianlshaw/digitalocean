cc78d4c8172406518d5d45db3b43e77660fc2eab
Ian Shaw <ian.shaw@thirtythree.co.uk>
2016-04-21 13:34:36 +0100

    ids arent strings ian

--- droplet_kit/create_loadbalancers.rb
+++ droplet_kit/create_loadbalancers.rb
@@ -10,5 +10,5 @@ lb01 = DropletKit::Droplet.new(name: 'loadbalancer01', region: 'lon1', size: '51
 lb01_result = client.droplets.create(lb01)
 lb02 = DropletKit::Droplet.new(name: 'loadbalancer02', region: 'lon1', size: '512mb', image: 'CentOS-6-5-x64')
 lb02_result = client.droplets.create(lb02)
-puts "loadbalancer01 id: " + lb01_result.id
-puts "loadbalancer02 id: " + lb02_result.id
+puts "loadbalancer01 id: " + lb01_result.id.to_s
+puts "loadbalancer02 id: " + lb02_result.id.to_s
