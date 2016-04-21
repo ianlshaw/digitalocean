28a29a93f634b3b0841bda647435a268261477b4
Ian Shaw <ian.shaw@thirtythree.co.uk>
2016-04-21 09:34:41 +0100

    added 3 base methods

--- droplet_kit/create_droplet.rb
+++ droplet_kit/create_droplet.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
-
 require 'droplet_kit'
-token='da8aac7272e7c0e4c5ecf76f5da1e0d651c5acfce867b61b3fe6f8d40e7cb131'
-client = DropletKit::Client.new(access_token: token)
 
+file = File.open('api.key')
+token = file.read
+client = DropletKit::Client.new(access_token: token)
 droplet = DropletKit::Droplet.new(name: 'example.com', region: 'nyc3', size: '1gb', image: 'ubuntu-14-04-x64')
 client.droplets.create(droplet)
--- droplet_kit/delete_droplet.rb
+++ droplet_kit/delete_droplet.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+require 'droplet_kit'
 
 USAGE='delete_droplet <id>'
 
@@ -7,9 +8,8 @@ if ARGV.length != 1
   exit(1)
 end
 
-require 'droplet_kit'
-token='da8aac7272e7c0e4c5ecf76f5da1e0d651c5acfce867b61b3fe6f8d40e7cb131'
+file = File.open('api.key')
+token = file.read
 client = DropletKit::Client.new(access_token: token)
-
 id = ARGV[0]
 client.droplets.delete(id: id)
