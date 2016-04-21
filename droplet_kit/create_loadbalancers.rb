2b472f47ac4984e762c3442e40f69b6f4fdd7660
Ian Shaw <ian.shaw@thirtythree.co.uk>
2016-04-21 13:30:20 +0100

    added create_loadbalancers.rb

---
+++ droplet_kit/create_loadbalancers.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+require 'droplet_kit'
+
+USAGE='create_loadbalancers'
+
+file = File.open('api.key')
+token = file.read
+client = DropletKit::Client.new(access_token: token)
+lb01 = DropletKit::Droplet.new(name: 'loadbalancer01', region: 'lon1', size: '512mb', image: 'CentOS-6-5-x64')
+lb01_result = client.droplets.create(lb01)
+lb02 = DropletKit::Droplet.new(name: 'loadbalancer02', region: 'lon1', size: '512mb', image: 'CentOS-6-5-x64')
+lb02_result = client.droplets.create(lb02)
+puts "loadbalancer01 id: " + lb01_result.id
+puts "loadbalancer02 id: " + lb02_result.id
